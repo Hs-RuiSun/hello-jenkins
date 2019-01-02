e70b6a26312a08ecd2ef6303a93694c2f7d36da2
ruby.sun <ruby.sun@cgi.com>
2019-01-02 16:41:18 -0400

    init

---
+++ src/main/java/com/rui/sun/HelloJenkins.java
@@ -0,0 +1,8 @@
+package com.rui.sun;
+
+public class HelloJenkins {
+    String message = "hello";
+    public void print(){
+        System.out.println(message);
+    }
+}
---
+++ src/test/java/com/rui/sun/HelloJenkinsTest.java
@@ -0,0 +1,13 @@
+package com.rui.sun;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloJenkinsTest {
+
+    @Test
+    void testPrint() {
+        assertEquals("hello", "hello");
+    }
+}
